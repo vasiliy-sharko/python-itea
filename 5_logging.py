9e4e998dc4ae9e8ef3992ab2375d0eda2d627cba
vasiliy-sharko <32044625+vasiliy-sharko@users.noreply.github.com>
2020-10-16 10:57:18 +0300

    Create 5_logging.py

---
+++ 5_logging.py
@@ -0,0 +1,11 @@
+import logging
+
+# logging.info("The world is round!")
+# logging.warning("Cats can bite you!")
+
+# logging.basicConfig(filename="test.log", filemode="a", level=logging.DEBUG)
+# logging.debug("Looking for bugs...")
+# logging.error("Bugs found!")
+
+# logging.basicConfig(format='%(asctime)s %(message)s')
+# logging.warning('is when this event was logged.')
